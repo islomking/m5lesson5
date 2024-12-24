d5aed8d14fed47fb153ad3203e69856875355359
islomking <yesuzb000@gmail.com>
2024-12-24 17:41:18 +0500

    Add files via upload

---
+++ s.js
@@ -0,0 +1,13 @@
+let ot = prompt("ismingizni kiriting")
+
+let space = prompt("space di idni kiriting")
+
+let password = prompt("space dagi passwordingiz")
+
+let all = {
+    name: `${ot}`,
+    id: `${space}`,
+    password: `${password}`
+};
+
+console.log(all);
